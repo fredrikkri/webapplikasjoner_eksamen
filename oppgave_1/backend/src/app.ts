d24b0069aa267abfb6d192980821f1fada8761a6
fredrikkri <fjkri@hotmail.com>
2024-11-06 11:43:11 +0100

    la til basics på server

--- oppgave_1/backend/src/app.ts
+++ oppgave_1/backend/src/app.ts
@@ -1,9 +1,18 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
+import { port } from "./config";
 
 const app = new Hono();
 
-app.use("/*", cors());
+app.use("/*", cors({
+  origin: "http://localhost:"+port,
+  credentials: true,
+}));
+
+
+app.get("/", (c) => {
+  return c.json({ message: "Welcome to the Hono server!" });
+});
 
 app.onError((err, c) => {
   console.error(err);
--- oppgave_1/backend/src/index.ts
+++ oppgave_1/backend/src/index.ts
@@ -1,6 +1,5 @@
 import app from "./app";
 import { port } from "./config";
-
 import { serve } from "@hono/node-server";
 
 console.log(`Server is running on port ${port}`);
@@ -9,3 +8,4 @@ serve({
   fetch: app.fetch,
   port,
 });
+
