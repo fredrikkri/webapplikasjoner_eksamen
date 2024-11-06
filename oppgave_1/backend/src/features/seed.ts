ae33305a384f118c92b7e8da68c9d10c22ccecc5
mapclean9r <tobias.the@hotmail.no>
2024-11-06 11:39:28 +0100

    database added ++ transformed data into  json

---
+++ oppgave_1/backend/src/features/seed.ts
@@ -0,0 +1,85 @@
+import { DB } from "./db";
+import { fileURLToPath } from "url";
+import { join } from "path";
+import { promises } from "fs";
+
+const __filename = fileURLToPath(import.meta.url);
+
+export const seed = async (db: DB) => {
+
+const path = join(".", "src", "features", "data", "data.json");
+const file = await promises.readFile(path, "utf-8");
+const { courses, comments, users, categories } = JSON.parse(file);
+
+  const insertCourse = db.prepare(`
+    INSERT INTO courses (id, title, slug, description, category)
+    VALUES (?, ?, ?, ?, ?)
+  `);
+
+  const insertLesson = db.prepare(`
+    INSERT INTO lessons (id, title, slug, preAmble, course_id)
+    VALUES (?, ?, ?, ?, ?)
+  `);
+
+  const insertText = db.prepare(`
+    INSERT INTO texts (text, lesson_id)
+    VALUES (?, ?)
+  `);
+
+  const insertComment = db.prepare(`
+    INSERT INTO comments (createdBy, comment, lesson_id)
+    VALUES (?, ?, ?)
+  `);
+
+  const insertUser = db.prepare(`
+    INSERT INTO users (id, name, email)
+    VALUES (?, ?, ?)
+  `);
+
+  // SRC: kilde: chatgpt.com /
+  const insertCategory = db.prepare(`
+    INSERT INTO categories (name) VALUES (?);
+  `);
+
+  // SRC: kilde: chatgpt.com || delvis /
+  db.transaction(() => {
+    for (const user of users) {
+      insertUser.run(user.id, user.name, user.email);
+    }
+
+    for (const category of categories) {
+        insertCategory.run(category);
+      }
+
+    for (const course of courses) {
+      insertCourse.run(
+        course.id,
+        course.title,
+        course.slug,
+        course.description,
+        course.category
+      );
+
+      for (const lesson of course.lessons) {
+        insertLesson.run(
+          lesson.id,
+          lesson.title,
+          lesson.slug,
+          lesson.preAmble,
+          course.id
+        );
+
+
+        for (const text of lesson.text) {
+          insertText.run(text.text, lesson.id);
+        }
+
+        for (const comment of comments) {
+          if (comment.lesson.slug === lesson.slug) {
+            insertComment.run(comment.createdBy.name, comment.comment, lesson.Id);
+          }
+        }
+      }
+    }
+  })();
+};
