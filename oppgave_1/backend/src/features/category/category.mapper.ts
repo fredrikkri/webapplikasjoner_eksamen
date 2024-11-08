6814cc6dc610e26d80c89a5780d0ad425cbfb563
mapclean9r <tobias.the@hotmail.no>
2024-11-08 12:46:21 +0100

    added utils

--- oppgave_1/backend/src/features/category/category.mapper.ts
+++ oppgave_1/backend/src/features/category/category.mapper.ts
@@ -1,18 +1,15 @@
 import { Categories } from "@/types/category";
 
 
-const createId = () => {
-    return crypto.randomUUID();
-  };
 
-  export const createCategoryResponse = (data: Categories): Categories => {
-    const { name } = data;
-      
-    return {
-      ...data,
-      name
-        };
-  };
+export const createCategoryResponse = (data: Categories): Categories => {
+  const { name } = data;
+    
+  return {
+    ...data,
+    name
+    };
+};
 
 export const fromDb = (data: Categories) => {
     return {
