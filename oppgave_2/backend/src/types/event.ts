62bccc7ff911eec132c07640f34685eb7741d02c
fredrikkri <fjkri@hotmail.com>
2024-11-11 14:17:46 +0100

    big

--- oppgave_2/backend/src/types/event.ts
+++ oppgave_2/backend/src/types/event.ts
@@ -2,12 +2,20 @@ import { z } from 'zod';
 
 export const EventSchema = z.object({
     id: z.string(),
-    name: z.string()
+    title: z.string(),
+    description: z.string(),
+    slug: z.string(),
+    date: z.date(),
+    location: z.string(),
+    event_type: z.string(),
+    total_slots: z.number(),
+    available_slots: z.number(),
+    price: z.number()
+
   })
   
   export type Event = z.infer<typeof EventSchema>
 
-  export const validateCategories = (data: unknown) => {
+  export const validateEvent = (data: unknown) => {
     return EventSchema.safeParse(data);
   };
-  
