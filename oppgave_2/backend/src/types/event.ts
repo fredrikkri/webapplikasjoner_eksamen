01f2e192dc142b4d145c01db5c282d45a17ffc92
mapclean9r <tobias.the@hotmail.no>
2024-11-11 14:32:20 +0100

    event type added

--- oppgave_2/backend/src/types/event.ts
+++ oppgave_2/backend/src/types/event.ts
@@ -11,10 +11,20 @@ export const EventSchema = z.object({
     total_slots: z.number(),
     available_slots: z.number(),
     price: z.number()
-
   })
   
+  export const EventCreateSchema = EventSchema.omit( {
+    id: true, available_slots: true
+  })
+
+  export const EventUpdateSchema = EventSchema.omit( {
+    id: true
+  })
+
   export type Event = z.infer<typeof EventSchema>
+  export type EventCreate = z.infer<typeof EventCreateSchema>
+  export type EventUpdate = z.infer<typeof EventUpdateSchema>
+  export type EventResponse = z.infer<typeof EventSchema>
 
   export const validateEvent = (data: unknown) => {
     return EventSchema.safeParse(data);
