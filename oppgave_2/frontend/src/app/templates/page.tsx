db734c4756a7e677eaa5ba9c16d0bcc3ee5006f3
fredrikkri <fjkri@hotmail.com>
2024-11-14 11:35:05 +0100

    ordan på mappestruktur

--- oppgave_2/frontend/src/app/templates/page.tsx
+++ oppgave_2/frontend/src/app/templates/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Home from "@/components/Home";
-import ListTemplates from "@/components/ListTemplates";
+import ListTemplates from "../../components/templates/ListTemplates";
 
 export default function Templates() {
     return(
