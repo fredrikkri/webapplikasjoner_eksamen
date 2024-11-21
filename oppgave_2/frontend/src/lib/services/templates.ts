0144be770121865d618d33b904ccf33ef406550e
fredrikkri <fjkri@hotmail.com>
2024-11-21 10:49:44 +0100

    big

--- oppgave_2/frontend/src/lib/services/templates.ts
+++ oppgave_2/frontend/src/lib/services/templates.ts
@@ -1,4 +1,4 @@
-import { BASE_URL, ENDPOINTS } from "@/config/config";
+import { BASE_URL, ENDPOINTS } from "../../config/config";
 import { Template } from "../../types/Template"
 
 export const getTemplate = async (slug: string): Promise<Template | undefined> => {
@@ -28,3 +28,25 @@ export const getAllTemplates = async (): Promise<Template[]> => {
   return result.data as Template[];
 };
 
+export const onAddTemplate = async ({ event_id }: { event_id: string }) => {
+  try {
+    const response = await fetch(ENDPOINTS.createTemplate, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ event_id }),
+    });
+
+    const data = await response.json();
+    if (!data.success) {
+      console.log("FAIL: ", data.data);
+      return;
+    }
+    return data;
+  } catch (error) {
+    console.log("fail catch");
+  } finally {
+    console.log("finally");
+  }
+};
