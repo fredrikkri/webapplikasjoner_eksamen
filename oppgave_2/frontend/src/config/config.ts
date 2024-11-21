92b479330599f9d3d0f508aecb5c8cbbf863c8b3
fredrikkri <fjkri@hotmail.com>
2024-11-21 13:42:06 +0100

    fiksa bug

--- oppgave_2/frontend/src/config/config.ts
+++ oppgave_2/frontend/src/config/config.ts
@@ -6,10 +6,6 @@ export const ENDPOINTS = {
   events: `${BASE_URL}/activeevents`,
   templates: `${BASE_URL}/templates`,
   create: `${BASE_URL}/create`,
-<<<<<<< HEAD
-  createRegistration: `${BASE_URL}/registrer`
-=======
+  createRegistration: `${BASE_URL}/registrer`,
   createTemplate: `${BASE_URL}/templates/add`,
-  createActiveEvent: `${BASE_URL}/activeevents/add`
->>>>>>> parent of 5c17744 (Merge branch 'oppg2_f_branch')
 };
